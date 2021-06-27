5b5165e88524177660c3bb5b7cef1832cace8a19
kevskillz <kevskillz10@gmail.com>
2021-06-27 11:04:14 -0400

    commit

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, url_for, request
 
-
 import pickle
 
 
@@ -8,17 +7,17 @@ import pickle
 def predict_conditions(age, gender, told_to_exercise, bmi, water, weight_kg, country):
     conditions = {
         'DLQ010': 'Serious difficulty hearing',
-        'DLQ020': 'Serious Difficulty seeing',
+        'DLQ020': 'Serious difficulty seeing',
         'DLQ040': 'Serious difficulty concentrating, remembering or making decisions',
         'DLQ050': 'Serious difficulty walking or climbing',
-        'DLQ060': 'difficulty dressing or bathing',
+        'DLQ060': 'Difficulty dressing or bathing',
 
         'CDQ001': 'Pain or discomfort in chest',
-        'CDQ008': 'severe pain across chest',
+        'CDQ008': 'Severe pain across chest',
         'CDQ010': 'Shortness of breath',
 
-        'BPQ020': 'hypertension/high blood pressure',
-        'BPQ080': 'high blood cholesterol level',
+        'BPQ020': 'Hypertension/high blood pressure',
+        'BPQ080': 'High blood cholesterol level',
 
         'AGQ030': 'Hay fever',
         'MCQ010': 'Asthma',
@@ -36,6 +35,7 @@ def predict_conditions(age, gender, told_to_exercise, bmi, water, weight_kg, cou
         'MCQ160K': 'Chronic Bronchitis',
 
     }
+
     in_us = 0
     t_o_e = 0
     gen = 0  # female
@@ -49,30 +49,42 @@ def predict_conditions(age, gender, told_to_exercise, bmi, water, weight_kg, cou
     retArr = []
     for k in conditions:
         model = pickle.load(open(f"{k}.pickle", "rb"))
-        if model.predict([[age, gen, t_o_e, bmi, water, weight_kg, in_us]])[0] == 1.:
+        s = model.predict([[age, gen, t_o_e, bmi, water, weight_kg, in_us]])[0]
+
+        if s == 1.:
             retArr.append(conditions.get(k))
     return retArr
-app = Flask(__name__,template_folder='template')
+
+
+app = Flask(__name__, template_folder='template')
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-    
-@app.route('/predict', methods=['POST'])
-def predict(): 
 
+@app.route('/predict', methods=['POST'])
+def predict():
     if request.method == 'POST':
-        country=request.form["country"]
-        age =request.form["age"]
+        country = request.form["country"]
+        age = request.form["age"]
         exercise = request.form["exercise"]
         weight = request.form["weight"]
         BMI = request.form["BMI"]
         gender = request.form["gender"]
         water = request.form["water"]
-        arr = predict_conditions(age, gender, exercise, BMI,water,weight,country )
 
-    return render_template('result.html',  results = arr)
+        arr = predict_conditions(age, gender, exercise, BMI, water, weight, country)
+        res = ""
+        for i in range(len(arr) - 1):
+            res += arr[i] + ', '
+        if len(arr) > 0:
+            res += arr[-1]
+        else:
+            res = "You look healthy!"
+    return render_template('result.html', results= res)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
