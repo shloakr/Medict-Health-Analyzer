07c8b89fb57f370d32b8df76d6bb45dfb0788f68
shloakr <shloakrathod1@gmail.com>
2021-06-27 13:23:13 +0530

    initial commit2

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 from flask import Flask, render_template, url_for, request
-import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+
 
 import pickle
 
